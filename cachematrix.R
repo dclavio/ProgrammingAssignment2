fe51432fe07a7aec372ad336f51fbd237be99926
Donald W. Clavio <dclavio@verizon.net>
2016-06-12 11:53:09 -0400

    Commiting the scoping assignment solution.

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,31 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeTheCacheMatrix <- function(mat = matrix()) {
+  inv <- NULL
+  set <- function(mat_again) 
+  {
+    mat <<- mat_again
+    inv <<- NULL
+  }
+  get <- function() mat
+  setInv <- function(solve) inv <<- solve
+  getInv <- function() inv
+  list(set=set, get=get,setInv = setInv, getInv = getInv)
 }
 
 
 ## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
+cacheSolve <- function(specialM, ...) {
         ## Return a matrix that is the inverse of 'x'
+  inv  <- specialM$getInv()
+  if(!is.null(inv)) {
+    message("returning cached inverse")
+    return(inv)
+  }
+  data <- specialM$get()
+  inv <- solve(data, ...)
+  specialM$setInv(inv)
+  inv
 }
